8f677a8b76b728b9887fffe9955d747d4a749f8f
FilipCysEk <krzysiek.swal@gmail.com>
2019-11-02 19:34:38 +0100

    Initial commit

---
+++ include/FileLib.h
@@ -0,0 +1,104 @@
+//
+// Created by cysiu on 20.10.2019.
+//
+
+#ifndef LIBRARIES1_FILELIB_H
+#define LIBRARIES1_FILELIB_H
+#include <iostream>
+#include <experimental/filesystem>
+#include <string>
+#include <chrono>
+
+using namespace std;
+namespace fs = experimental::filesystem;
+
+
+class FileLib {
+public:
+    /**
+     * @brief Convert relative file path to static file path
+     * @param path relative path to file/directory
+     * @return static path to file/directory (directory have on end '/')
+     */
+    string getFullPath(string path);
+
+    /**
+     * @brief CHeck existing of file/directory
+     * @param path static path to file/directory
+     * @return 1 - exists, 0 - no exist
+     */
+    bool fileExist(string path);
+
+    /**
+     * @brief Return file modification time
+     * @param path static path to file
+     * @return last modification time
+     */
+    time_t fileTime(string path);
+
+    /**
+     * @brief Creating directory
+     * @param name name of new directory
+     * @param path static path to directory, where directory be created
+     * @return 1 - successfully, 0 - error
+     */
+    bool createDirectory(string name, string path);
+    /**
+     * @brief Creating directory
+     * @param path static path to new directory
+     * @return 1 - successfully, 0 - error
+     */
+    bool createDirectory(string path);
+
+    /**
+     * @brief Remove file/directory
+     * @param path static path to removed file/directory
+     * @return 1 - successfully, 0 - error
+     */
+    bool removeFile(string path);
+
+    /**
+     * @brief Remove all files from directory
+     * @param path static file to directory
+     * @return 1 - successfully, 0 - error
+     */
+    bool emptyingDirectory(string path);
+
+    /**
+     * @brief rename file/directory
+     * @param oldName old name of file
+     * @param newName new name of file
+     * @param path static path to directory, where this file is
+     */
+    void rename(string oldName, string newName, string path);
+
+    /**
+     * @brief Rename file/directory
+     * @param oldFilePath static path to old file with name
+     * @param newFilePath static path to new file with name
+     */
+    void rename(string oldFilePath, string newFilePath);
+
+    /**
+     * @brief Copy file/firectory
+     * @param oldPath static path to source file
+     * @param newPath static path to paste file
+     */
+    void copy(string oldPath, string newPath);
+
+    /**
+     * @brief Move file/directory
+     * @param oldPath static path to actually file
+     * @param newPath static path to new file location
+     */
+    void move(string oldPath, string newPath);
+
+
+private:
+    bool isStaticPath(string path);
+    bool isStaticDirPath(string path);
+
+};
+
+
+#endif //LIBRARIES1_FILELIB_H
---
+++ src/FileLib.cpp
@@ -0,0 +1,120 @@
+//
+// Created by cysiu on 20.10.2019.
+//
+
+#include "../include/FileLib.h"
+
+string FileLib::getFullPath(string path) {
+    if (path.find('\\') == string::npos) {
+        if (path.size() != 0) {
+            //Passed absolute path?
+            if (!(path[0] == '/' || (path[1] == ':' && path[2] == '/'))) {
+                path = fs::current_path().string() + '/' + path;
+            }
+            //Checking last sign is /
+            if (path[path.size() - 1] == '/')
+                return path;
+            else {
+                for( int p = path.size() - 1; p > 0; p--){
+                    if(path[p] == '/') break;
+
+                    if(path[p] == '.') return path;
+                }
+                return path + "/";
+            }
+        } else {
+            return fs::current_path().string() + '/';
+        }
+    } else {
+        throw "Unexcepted \\ in path";
+    }
+}
+
+bool FileLib::fileExist(string path) {
+    if(isStaticPath(path))
+        return fs::exists(path);
+    else
+        throw "Passed relative path";
+}
+
+time_t FileLib::fileTime(string path) {
+    if(isStaticPath(path)){
+        auto fileTime = fs::last_write_time(path);
+        return decltype(fileTime)::clock::to_time_t(fileTime);
+    }
+    else
+        throw "Passed relative path";
+}
+
+bool FileLib::isStaticPath(string path) {
+    return path[0] == '/' || (path[1] == ':' && path[2] == '/');
+}
+
+bool FileLib::isStaticDirPath(string path) {
+    return (path[0] == '/' || (path[1] == ':' && path[2] == '/')) && path[path.size() - 1] == '/';
+}
+
+bool FileLib::createDirectory(string name, string path) {
+    if(isStaticDirPath(path)){
+        createDirectory(path + name);
+    }
+    else
+        throw "Passed relative path or file path";
+}
+
+bool FileLib::createDirectory(string path) {
+    if(isStaticPath(path)){
+        return fs::create_directories(path);
+    }
+    else
+        throw "Passed relative path";
+}
+
+bool FileLib::removeFile(string path) {
+    if(isStaticPath(path)){
+        return fs::remove_all(path);
+    }
+    else
+        throw "Passed relative path";
+}
+
+bool FileLib::emptyingDirectory(string path) {
+    if(isStaticPath(path)){
+        short res = 0;
+        res += fs::remove_all(path);
+        res += createDirectory(path);
+        return res == 2;
+    }
+    else
+        throw "Passed relative path";
+}
+
+void FileLib::rename(string oldName, string newName, string path) {
+    if(isStaticPath(path)){
+        rename(path + oldName, path + newName);
+    }
+    else
+        throw "Passed relative path";
+}
+
+void FileLib::rename(string oldFilePath, string newFilePath) {
+    if(isStaticPath(oldFilePath) && isStaticPath(newFilePath)){
+        fs::rename(oldFilePath, newFilePath);
+    }
+    else
+        throw "Passed relative path";
+}
+
+void FileLib::copy(string oldPath, string newPath) {
+    if(isStaticPath(oldPath) && isStaticPath(newPath)){
+        fs::copy(oldPath, newPath);
+    }
+    else
+        throw "Passed relative path";
+}
+
+void FileLib::move(string oldPath, string newPath) {
+    rename(oldPath, newPath);
+}
+
+
